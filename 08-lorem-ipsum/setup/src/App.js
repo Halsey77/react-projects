b24d25a7c895316339f01b4a3683b51efc26c114
Halsey77 <provcc143300@gmail.com>
2022-02-15 21:22:41 +0700

    complete project 8

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -1,11 +1,38 @@
-import React, { useState } from 'react';
-import data from './data';
+import React, { useState } from "react";
+import data from "./data";
 function App() {
-  
-  
+  const [count, setCount] = useState(1);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setText(data.slice(0, count));
+  };
+
   return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    <section className="section-center">
+      <h3>Tired of boring lorem ispum?</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">paragraphs:</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          value={count}
+          onChange={(event) => setCount(event.target.value)}
+          min="1"
+        ></input>
+        <button type="submit" className="btn">
+          Generate text
+        </button>
+      </form>
+      <article className="lorem-text">
+        {text.map((t, index) => (
+          <p key={index}>{t}</p>
+        ))}
+      </article>
+    </section>
+  );
 }
 
 export default App;
